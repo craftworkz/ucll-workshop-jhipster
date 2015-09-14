9124175039bacb2cdc92f2fd8b8c96f247ac3a36
Driss Amri <driss.amri@telenet.be>
2015-09-14 19:08:14 +0200

    Initial Spring Boot sources (start.spring.io)

---
+++ demo-spring-boot/spring-boot-ucll/src/main/java/be/craftworkz/ucll/SpringBootUcllDemoApplication.java
@@ -0,0 +1,12 @@
+package be.craftworkz.ucll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUcllDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUcllDemoApplication.class, args);
+    }
+}
